5674b06c53031b1e258cff5db1bad39a3c9012ef
Cyril <cyril78@gmail.com>
2011-12-29 22:08:36 +0100

    clean code

--- gen/fr/sosmessagedecarte/R.java
+++ gen/fr/sosmessagedecarte/R.java
@@ -22,11 +22,10 @@ public final class R {
         public static final int logo=0x7f020005;
         public static final int logo_anniv=0x7f020006;
         public static final int logo_dashboard=0x7f020007;
-        public static final int logo_mariage=0x7f020008;
-        public static final int logo_pot=0x7f020009;
-        public static final int logo_remerciement=0x7f02000a;
-        public static final int old2_ic_launcher=0x7f02000b;
-        public static final int old_ic_launcher=0x7f02000c;
+        public static final int logo_pot=0x7f020008;
+        public static final int logo_remerciement=0x7f020009;
+        public static final int old2_ic_launcher=0x7f02000a;
+        public static final int old_ic_launcher=0x7f02000b;
     }
     public static final class id {
         public static final int InnerRelativeLayout=0x7f070000;
@@ -42,21 +41,23 @@ public final class R {
         public static final int anniv=0x7f030000;
         public static final int dashboard=0x7f030001;
         public static final int main=0x7f030002;
-        public static final int mariage=0x7f030003;
-        public static final int pot=0x7f030004;
-        public static final int voeux=0x7f030005;
+        public static final int pot=0x7f030003;
+        public static final int voeux=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040004;
-        public static final int button_text=0x7f040005;
-        public static final int mock_anniv=0x7f040003;
-        public static final int mock_mariage=0x7f040000;
-        public static final int mock_pot=0x7f040002;
-        public static final int mock_remerciement=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int birthday_category=0x7f040002;
+        public static final int break_up_category=0x7f040006;
+        public static final int button_text=0x7f040001;
+        public static final int leaving_party_category=0x7f040003;
+        public static final int valentine_s_day_category=0x7f040005;
+        public static final int wish_category=0x7f040004;
     }
     public static final class style {
         /**  item name="android:windowBackground">@drawable/handmadepaper</item
          */
         public static final int SosTheme=0x7f050000;
+        public static final int category=0x7f050001;
+        public static final int logo=0x7f050002;
     }
 }
